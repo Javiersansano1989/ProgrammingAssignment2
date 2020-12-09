1d4a1a221589964fc03de70d5f4ea54326c407e4
Javiersansano1989 <javier.lopezllumnos.usm.cl>
2020-12-09 17:30:44 -0300

    change of forked depository

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mx = matrix()){ #as per instructions, and defining the matrix as such
+  
+  inv_mx <- NULL # following the example of makeVector, this line creates an empty object to store the solution into.
+  
+  set <- function(ext_mx) { # this 'set' function assigns the a dummy object into matrix
+    
+    mx <<- ext_mx # the dummy object is called 'external matrix', as the <<- is used to assign values in a diferrent environment, or 'external'
+    inv_mx <<- NULL
+    
+  }
+  
+  get <- function() mx # whereas set assigns the dummy object into the 'main' object, get 'grab' the object
+  set_inv_mx <- function(ext_inv_mx) inv_mx <<- ext_inv_mx # and just like with the mx and ext_mx, the function set_inv_mx assgigns an external or dummy inverted matrix into the solution
+  get_inv_mx <- function() inv_mx # and just like with the mx and ext_mx, the function set_inv_mx assgigns an external or dummy inverted matrix into the solution
+  list(set = set, get = get, set_inv_mx = set_inv_mx, get_inv_mx = get_inv_mx) #finally, just like with makeVector, the BIG function is defined as a list of functions to 'create' and 'grab' both the 'main objects' (original and inverted matrixes) and and the 'dummies'
+  
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mx, ...) {
+  
+  inv_mx <- mx$get_inv_mx() # following the example of cachemean, the function for grabbing the 'dummy' inverted matrix is called
+  
+  if(!is.null(inv_mx)){ # if the code has already run, then it's better to go get the inverted matrix within the cache instead of "solving" the problem each and every time
+    
+    message("getting cached data")
+    return(inv_mx)
+    
+  }
+  
+  data <- mx$get() # the matrix is "deposited" into 'data', effectively going into the cache
+  inv_mx <- solve(data) # whereas now data is "solved"; as per cran.r-project.org: "... the standard R function for matrix inverse is solve()"
+  mx$set_inv_mx(inv_mx)
+  inv_mx # prints the inverted matrix, wheter it was just calculated, or already existed within the cache
+  
 }
